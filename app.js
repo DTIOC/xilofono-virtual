d17b1c31a617d0f8aaa6ddde805f290c990ee043
DTIOC <oscar.cardenas@iems.edu.mx>
2026-08-31 14:07:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class XylophoneApp {
         this.scaleNotes = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5'];
         
         // Tu webhook de Google Sheets
-        this.webhookURL = 'https://script.google.com/macros/s/AKfycbwDH28-ogn4VkjIjtaqPw-SQlj1cCvdfrByd_Q6E-VWf8-pe07fD0gtweto-ovz0nJ2/exec';
+        this.webhookURL = 'https://script.google.com/macros/s/AKfycbzFjmLuFfUtl6BPCsQ4ynRVBxmCcTGQ4Gope2VIwiibuMdaQ2eiaDPkCH22571U-rGQ/exec';
         
         this.currentLevel = 1;
         this.successStreak = 0;
